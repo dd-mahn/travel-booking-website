2960a6cc2168471aaa9d29a274ee319f6bb28734
godsadeser <ducmanh0801@gmail.com>
2023-10-23 01:55:19 +0700

    Set the limit of displayed tours

--- frontend/src/pages/HomeComponent/FeaturedTourList.jsx
+++ frontend/src/pages/HomeComponent/FeaturedTourList.jsx
@@ -4,10 +4,15 @@ import TourData from "../../assets/data/tours"
 import { Col } from 'reactstrap'
 
 const FeaturedTourList = () => {
+  let FeaturedTourData = []
+  for(let i = 0; i < 6; i++){
+    FeaturedTourData.push(TourData[i])
+  }
+
   return (
     <>
     {
-        TourData?.map(tour=> (
+        FeaturedTourData?.map(tour=> (
             <Col lg='3' className='mb-4' key={tour.id}>
                 <TourCard tour={tour}/>
             </Col>
