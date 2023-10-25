b0a5ef8162674cd8549d9c5a288361563e1f98ab
godsadeser <ducmanh0801@gmail.com>
2023-10-25 17:28:31 +0700

    Finish thank page

---
+++ frontend/src/pages/ThankYou.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Container, Row, Col, Button } from 'reactstrap'
+import { Link } from 'react-router-dom'
+import '../styles/thank-you.css'
+
+
+const ThankYou = () => {
+  return <section>
+    <Container>
+        <Row>
+            <Col lg='12' className='pt-5 text-center'>
+                <div className="thank-you">
+                    <span>
+                        <i class="ri-checkbox-circle-line"></i>
+                        <h1 className="mb-3 fw-semibold">Thank you</h1>
+                        <h3 className="mb-3">Tour booked successfully!</h3>
+
+                        <Button className='primary__btn w-25'><Link to='/home'>Back to home</Link></Button>
+                    </span>
+                </div>
+            </Col>
+        </Row>
+    </Container>
+  </section>
+}
+
+export default ThankYou
