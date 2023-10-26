c71ec0aee24917855e5beee0724b2ffdd9104f26
godsadeser <ducmanh0801@gmail.com>
2023-10-26 17:14:42 +0700

    Finish all req method

--- backend/controllers/tourController.js
+++ backend/controllers/tourController.js
@@ -18,37 +18,78 @@ export const createTour = async (req,res) => {
 
 //update tour
 export const updateTour = async(req,res) => {
+
+    const id = req.params.id
+
     try {
-        
+        const updatedTour = await Tour.findByIdAndUpdate(id, {
+            $set: req.body
+        }, {new:true})
+
+        res.status(200).json({success:true, message:'Successfully updated', data:updatedTour})
     } catch (error) {
+        res.status(500).json({success:false, message:'Failed to update!'})
         
     }
 }
 
 //delete tour
 export const deleteTour = async(req,res) => {
+    const id = req.params.id
+
     try {
-        
+        await Tour.findByIdAndDelete(id)
+
+        res.status(200).json({success:true, message:'Successfully deleted'})
     } catch (error) {
+        res.status(500).json({success:false, message:'Failed to delete!'})
         
     }
 }
 
 //get single tour
 export const getSingleTour = async(req,res) => {
+    const id = req.params.id
+
     try {
-        
+        const tour = await Tour.findById(id)
+
+        res.status(200).json({success:true, message:'Successful', data:tour})
     } catch (error) {
+        res.status(404).json({success:false, message:'Not found!'})
         
     }
 }
 
 //get all tour
 export const getAllTour = async(req,res) => {
+
+    //for pagination
+    const page = parseInt(req.query.page)
+
+    console.log(page)
+
     try {
-        
+        const tours = await Tour.find({}).skip(page*8).limit(8)
+
+        res.status(200).json({success:true, count:tours.length, message:'Successful', data:tours})
     } catch (error) {
+        res.status(404).json({success:false, message:'Not found!'})
         
     }
 }
 
+//get tour by searching
+
+export const getTourBySearch = async(req,res)=>{
+    const city = new RegExp(req.query.city, 'i')// i - case sensitive
+    const distance = parseInt(req.query.distance)
+    const maxGroupSize = parseInt(req.query.maxGroupSize)
+
+    try {
+        const tours = await Tour.find({city, distance:{$gte:distance}, maxGroupSize:{$gte:maxGroupSize}})
+        res.status(200).json({success:true, message:'Successful', data:tours})
+    } catch (error) {
+        res.status(404).json({success:false, message:'Not found!'})
+    }
+}
