ac8ce862dfe23b7445830723fa82ee7df24eb752
godsadeser <ducmanh0801@gmail.com>
2023-10-26 17:15:01 +0700

    Add all req method

--- backend/routes/tours.js
+++ backend/routes/tours.js
@@ -1,9 +1,23 @@
 import express from 'express'
-import {createTour} from '../controllers/tourController.js'
+import {createTour, updateTour, deleteTour, getSingleTour, getAllTour, getTourBySearch} from '../controllers/tourController.js'
 
 const router = express.Router()
 
 //create new tour
 router.post("/", createTour)
 
+//update tour
+router.put("/:id", updateTour)
+
+//delete tour
+router.delete("/:id", deleteTour)
+
+//get single tour
+router.get("/:id", getSingleTour)
+
+//get all tour
+router.get("/", getAllTour)
+
+//get tour by searching
+router.get('/search/getTourBySearch', getTourBySearch)
 export default router
