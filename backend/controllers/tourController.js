d5ffc6a3145b0c2f9246f46ab08f185e7fe06d20
godsadeser <ducmanh0801@gmail.com>
2023-10-28 15:59:19 +0700

    add more controllers, handle whitespaces in search

--- backend/controllers/tourController.js
+++ backend/controllers/tourController.js
@@ -82,14 +82,44 @@ export const getAllTour = async(req,res) => {
 //get tour by searching
 
 export const getTourBySearch = async(req,res)=>{
-    const city = new RegExp(req.query.city, 'i')// i - case sensitive
+    //handle whitespaces in input
+    const escapedCity = req.query.city.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')
+
+    //Input type
+    const city = new RegExp(escapedCity, 'i')// i - case sensitive
     const distance = parseInt(req.query.distance)
     const maxGroupSize = parseInt(req.query.maxGroupSize)
 
     try {
         const tours = await Tour.find({city, distance:{$gte:distance}, maxGroupSize:{$gte:maxGroupSize}})
-        res.status(200).json({success:true, message:'Successful', data:tours})
+        if(tours.length !== 0){
+            res.status(200).json({success:true, message:'Successful', data:tours})
+        }else{
+            res.status(404).json({success:false, message:'Not found!'})
+        }       
     } catch (error) {
         res.status(404).json({success:false, message:'Not found!'})
+    }
+}
+
+//get featured tour
+export const getFeaturedTour = async(req,res) => {
+    try {
+        const tours = await Tour.find({featured:true}).limit(8)
+
+        res.status(200).json({success:true, count:tours.length, message:'Successful', data:tours})
+    } catch (error) {
+        res.status(404).json({success:false, message:'Not found!'})
+        
+    }
+}
+
+//get tour count
+export const getTourCount = async(req,res) => {
+    try {
+        const tourCount = await Tour.estimatedDocumentCount()
+        res.status(200).json({success:true, data:tourCount})
+    } catch (error) {
+        res.status(500).json({success:false, message:'Failed to fetch'})
     }
 }
