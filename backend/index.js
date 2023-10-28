e60bea4b9323b22fc002848e9fb2fbb223553a9d
godsadeser <ducmanh0801@gmail.com>
2023-10-28 16:10:25 +0700

    Add user controller and routes

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,11 @@ import express from "express";
 import dotenv from 'dotenv'
 import mongoose from "mongoose";
 import cors from 'cors'
-import cookieParser from "cookie-parser";
+import cookieParser from "cookie-parser"
 
 import tourRoute from './routes/tours.js'
+import userRoute from './routes/users.js'
+
 
 dotenv.config()
 const app = express()
@@ -30,6 +32,8 @@ app.use(express.json())
 app.use(cors())
 app.use(cookieParser())
 app.use('/tours', tourRoute)
+app.use('/users', userRoute)
+
 
 app.listen(port, ()=> {
     connect()
