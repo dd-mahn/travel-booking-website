2b1c7aa9c10a1fc90af4cc0f0d6d8a1bfe4d730c
godsadeser <ducmanh0801@gmail.com>
2023-10-28 17:37:40 +0700

    Add auth use

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,8 @@ import cookieParser from "cookie-parser"
 
 import tourRoute from './routes/tours.js'
 import userRoute from './routes/users.js'
+import authRoute from './routes/auth.js'
+
 
 
 dotenv.config()
@@ -33,6 +35,8 @@ app.use(cors())
 app.use(cookieParser())
 app.use('/tours', tourRoute)
 app.use('/users', userRoute)
+app.use('/auth', authRoute)
+
 
 
 app.listen(port, ()=> {
