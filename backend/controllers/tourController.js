d64191e40ab65a6bb92cc0f2b856547ef73c361d
godsadeser <ducmanh0801@gmail.com>
2023-10-29 16:48:12 +0700

    add review show

--- backend/controllers/tourController.js
+++ backend/controllers/tourController.js
@@ -52,7 +52,7 @@ export const getSingleTour = async(req,res) => {
     const id = req.params.id
 
     try {
-        const tour = await Tour.findById(id)
+        const tour = await Tour.findById(id).populate('reviews')
 
         res.status(200).json({success:true, message:'Successful', data:tour})
     } catch (error) {
@@ -70,7 +70,7 @@ export const getAllTour = async(req,res) => {
     console.log(page)
 
     try {
-        const tours = await Tour.find({}).skip(page*8).limit(8)
+        const tours = await Tour.find({}).populate('reviews').skip(page*8).limit(8)
 
         res.status(200).json({success:true, count:tours.length, message:'Successful', data:tours})
     } catch (error) {
@@ -91,7 +91,7 @@ export const getTourBySearch = async(req,res)=>{
     const maxGroupSize = parseInt(req.query.maxGroupSize)
 
     try {
-        const tours = await Tour.find({city, distance:{$gte:distance}, maxGroupSize:{$gte:maxGroupSize}})
+        const tours = await Tour.find({city, distance:{$gte:distance}, maxGroupSize:{$gte:maxGroupSize}}).populate('reviews')
         if(tours.length !== 0){
             res.status(200).json({success:true, message:'Successful', data:tours})
         }else{
@@ -105,7 +105,7 @@ export const getTourBySearch = async(req,res)=>{
 //get featured tour
 export const getFeaturedTour = async(req,res) => {
     try {
-        const tours = await Tour.find({featured:true}).limit(8)
+        const tours = await Tour.find({featured:true}).limit(8).populate('reviews')
 
         res.status(200).json({success:true, count:tours.length, message:'Successful', data:tours})
     } catch (error) {
