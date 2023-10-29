d82b5c15b9afe5bbb9f6249cecb682ec646103f6
godsadeser <ducmanh0801@gmail.com>
2023-10-29 16:44:43 +0700

    Add verify

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,17 +1,19 @@
 import express from 'express'
-import { deleteUser, getAllUser, getSingleUser, updateUser } from '../controllers/userController'
+import { deleteUser, getAllUser, getSingleUser, updateUser } from '../controllers/userController.js'
 const router = express.Router()
 
+import { verifyAdmin, verifyUser } from '../utils/verifyToken.js'
+
 //update user
-router.put("/:id", updateUser)
+router.put("/:id", verifyUser, updateUser)
 
 //delete user
-router.delete("/:id", deleteUser)
+router.delete("/:id", verifyUser, deleteUser)
 
 //get single user
-router.get("/:id", getSingleUser)
+router.get("/:id", verifyUser, getSingleUser)
 
 //get all user
-router.get("/", getAllUser)
+router.get("/", verifyAdmin, getAllUser)
 
 export default router
