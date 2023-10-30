3d3126141e268dd3997710a54d37cc306054d61b
godsadeser <ducmanh0801@gmail.com>
2023-10-30 17:09:15 +0700

    add booking routes

--- backend/routes/booking.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { verifyUser } from '../utils/verifyToken.js'
-import { createBooking } from '../controllers/bookingController.js'
-
-const router = express.Router()
-
-router.post('/:tourId', verifyUser, createBooking )
-
-export default router
