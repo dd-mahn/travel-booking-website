6980ba4e10829e644fb0b96111435956eae12cf1
godsadeser <ducmanh0801@gmail.com>
2023-10-30 17:09:43 +0700

    add booking api

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,8 @@ import tourRoute from './routes/tours.js'
 import userRoute from './routes/users.js'
 import authRoute from './routes/auth.js'
 import reviewRoute from './routes/reviews.js'
+import bookingRoute from './routes/bookings.js'
+
 
 
 
@@ -43,6 +45,8 @@ app.use('/api/v1/tours', tourRoute)
 app.use('/api/v1/users', userRoute)
 app.use('/api/v1/auth', authRoute)
 app.use('/api/v1/review', reviewRoute)
+app.use('/api/v1/booking', bookingRoute)
+
 
 
 
