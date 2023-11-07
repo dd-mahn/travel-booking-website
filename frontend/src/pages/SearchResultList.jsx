7a730e94f3518a9ae45d1ca5f3ba510d1a94d490
godsadeser <ducmanh0801@gmail.com>
2023-11-07 16:18:07 +0700

    Finish layout and connect to apis

--- frontend/src/pages/SearchResultList.jsx
+++ frontend/src/pages/SearchResultList.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
+import CommonSection from '../shared/CommonSection'
+import { Container, Row, Col } from 'reactstrap'
+
+import { useLocation } from 'react-router-dom'
+import TourCard from '../shared/TourCard'
+
+import Newsletter from '../shared/Newsletter'
 
 const SearchResultList = () => {
+  const location = useLocation()
+
+  const [data] = useState(location.state)
+
   return (
-    <div>SearchResultList</div>
+    <>
+      <CommonSection title={'Search Result'}/>
+      <section>
+        <Container>
+          <Row>
+            {
+              data.length === 0 ? 
+              ( <h4 className='text-center'>No tours found!</h4>)
+              : ( data?.map(tour => (
+                <Col lg='3' className='mb-4' key={tour._id}>
+                  <TourCard tour={tour}/>
+                </Col>
+              )))
+            }
+          </Row>
+        </Container>
+      </section>
+      <Newsletter />
+    </>
   )
 }
 
