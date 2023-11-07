38ecda13cd51f0f6ee63f6277a4b8151961c1b98
godsadeser <ducmanh0801@gmail.com>
2023-11-07 16:50:49 +0700

    Connect to backend data

--- frontend/src/pages/Tours.jsx
+++ frontend/src/pages/Tours.jsx
@@ -3,19 +3,26 @@ import CommonSection from '../shared/CommonSection'
 import '../styles/tours.css'
 import { Container,Row,Col } from 'reactstrap'
 
-import tourData from '../assets/data/tours'
 import SearchBar from '../shared/SearchBar'
 import TourCard from '../shared/TourCard'
 import Newsletter from '../shared/Newsletter'
+
+import useFetch from '../hooks/useFetch'
+import { BASE_URL } from '../utils/config'
 
 const Tours = () => {
   const [pageCount, setPageCount] = useState(0)
   const [page, setPage] = useState(0)
 
+  const {data:tours, loading, error} = useFetch(`${BASE_URL}/tours?page=${page}`)
+  const {data:tourCount} = useFetch(`${BASE_URL}/tours/search/getTourCount`)
+
+
   useEffect(() => {
-    const pages = Math.ceil(5/4)
+    const pages = Math.ceil(tourCount/8)
     setPageCount(pages)
-  }, [page])
+    window.scrollTo(0,0)
+  }, [page, tourCount, tours])
 
   return (
     <>
@@ -29,10 +36,14 @@ const Tours = () => {
       </section>
       <section>
         <Container>
-          <Row>
+          {loading && <h4 className='text-center pt-5'>Loading...</h4>}
+          {error && <h4 className='text-center pt-5'>{error}</h4>}
+
+          {
+            !loading && !error && <Row>
             {
-              tourData?.map(tour => (
-              <Col lg='3' className='mb-4'><TourCard tour={tour} key={tour.id}/>
+              tours?.map(tour => (
+              <Col lg='3' className='mb-4'><TourCard tour={tour} key={tour._id}/>
               </Col>))
             }
 
@@ -46,6 +57,7 @@ const Tours = () => {
               </div>
             </Col>
           </Row>
+          }
         </Container>
       </section>
       <Newsletter/>
