1aeda82fc6974e0dcca760ba78acf0ca43a4c252
godsadeser <ducmanh0801@gmail.com>
2023-11-09 16:47:00 +0700

    Connect backend api

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,23 +1,47 @@
-import React from 'react'
+import React, {useState, useContext} from 'react'
 import {Container, Row, Col, Form, FormGroup, Button} from 'reactstrap'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import '../styles/login.css'
 
 import registerImg from '../assets/images/register.png'
-import { useState } from 'react'
+import { AuthContext } from '../context/AuthContext'
+import { BASE_URL } from '../utils/config'
 
-const Login = () => {
+const Register = () => {
+  const navigate = useNavigate()
+
   const [credentials, setCredentials] = useState({
+    username: undefined,
     email: undefined,
     password: undefined,
   })
+
+  const {dispatch} = useContext(AuthContext)
 
   const handleChange = e => {
     setCredentials(prev => ({...prev, [e.target.id]:e.target.value}))
   }
 
-  const handleClick = e => {
+  const handleClick = async e => {
     e.preventDefault()
+
+    try {
+      const res = await fetch(`${BASE_URL}/auth/register`, {
+        method:'post',
+        headers:{
+          'content-type':'application/json'
+        },
+        body: JSON.stringify(credentials)
+      })
+      const result = await res.json()
+
+      if(!res.ok)alert(result.message)
+
+      dispatch({type:'REGISTER_SUCCESS'})
+      navigate('/login')
+    } catch (err) {
+      alert(err.message)
+    }
   }
   return <section>
     <Container>
@@ -29,11 +53,14 @@ const Login = () => {
         </Col>
         <Col lg='6' className=''>
           <div className="login__container">
-            <div className="login__form d-flex flex-column align-items-center justify-content-center">
+            <div className="login__form d-flex flex-column align-items-center justify-content-center pt-5">
               
-              <h2>Register</h2>
+              <h2 className='mt-3'>Register</h2>
 
               <Form onSubmit={handleClick}>
+                <FormGroup className='w-100'>
+                  <input type="text" placeholder='Username' required id='username' onChange={handleChange} />
+                </FormGroup>
                 <FormGroup className='w-100'>
                   <input type="email" placeholder='Email' required id='email' onChange={handleChange} />
                 </FormGroup>
@@ -53,4 +80,4 @@ const Login = () => {
   </section>
 }
 
-export default Login
+export default Register
