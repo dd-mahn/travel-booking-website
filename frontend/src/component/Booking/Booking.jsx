25cc16b72f021859dc6a203cd1ffef6376bba99e
godsadeser <ducmanh0801@gmail.com>
2023-11-10 16:29:15 +0700

    Connect booking api

--- frontend/src/component/Booking/Booking.jsx
+++ frontend/src/component/Booking/Booking.jsx
@@ -1,16 +1,21 @@
-import React,{useState} from 'react'
+import React,{useState, useContext} from 'react'
 import './booking.css'
 import { Form, FormGroup, ListGroup, ListGroupItem, Button, Col } from 'reactstrap'
 import { useNavigate } from 'react-router-dom'
+import {AuthContext} from '../../context/AuthContext'
+import {BASE_URL} from '../../utils/config'
 
 const Booking = ({tour, avgRating}) => {
-    const {price, reviews} = tour
+    const {price, reviews, title} = tour
 
     const navigate = useNavigate()
 
-    const [credentials, setCredentials] = useState({
-        userId: '01',
-        userEmail: 'example@gmail.com',
+    const {user} = useContext(AuthContext)
+
+    const [booking, setBooking] = useState({
+        userId: user && user._id,
+        userEmail: user && user.email,
+        tourName: title,
         fullName: '',
         phone: '',
         guestSize: 1,
@@ -18,17 +23,42 @@ const Booking = ({tour, avgRating}) => {
     })
 
     const handleChange = e => {
-        setCredentials(prev => ({...prev, [e.target.id]:e.target.value}))
+        setBooking(prev => ({...prev, [e.target.id]:e.target.value}))
     }
 
     const serviceFee = 10
-    const totalAmount = Number(price) * Number(credentials.guestSize) + Number(serviceFee)
+    const totalAmount = Number(price) * Number(booking.guestSize) + Number(serviceFee)
 
     // Send data to server
-    const handleClick = e => {
+    const handleClick = async e => {
         e.preventDefault()
 
-        navigate('/thank-you')
+        try {
+            if(!user || user===undefined || user===null){
+                return alert('Please login first!')
+            }
+
+            const res = await fetch(`${BASE_URL}/booking`, {
+                method:'post',
+                headers:{
+                    'content-type':'application/json'
+                },
+                credentials:'include',
+                body:JSON.stringify(booking)
+            })
+
+            const result = await res.json()
+
+            if(! res.ok){
+                return alert(result.message)
+            }
+
+            navigate('/thank-you')
+            console.log(booking)  
+
+        } catch (error) {
+            alert(error.message)
+        }
     }
 
   return (
@@ -45,17 +75,17 @@ const Booking = ({tour, avgRating}) => {
         <h5>Information</h5>
         <Form className="booking__info-form" onSubmit={handleClick}>
             <FormGroup>
-                <input type="text" placeholder='Full Name' id='fullName' required onClick={handleChange} />
+                <input type="text" placeholder='Full Name' id='fullName' required onChange={handleChange} />
             </FormGroup>
             <FormGroup>
-                <input type="number" placeholder='Phone' id='phone' required onClick={handleChange} />
+                <input type="number" placeholder='Phone' id='phone' required onChange={handleChange} />
             </FormGroup>
             <FormGroup className='d-flex align-items-center justify-content-between gap-3'>
                 <Col lg='7'>
-                <input type="date" placeholder='' id='bookAt' required onClick={handleChange} />
+                <input type="date" placeholder='' id='bookAt' required onChange={handleChange} />
                 </Col>
                 <Col lg='4'>
-                <input type="number" placeholder='Guest' id='guestSize' required onClick={handleChange} />
+                <input type="number" placeholder='Guest' id='guestSize' required onChange={handleChange} />
                 </Col>
             </FormGroup>
         </Form>
