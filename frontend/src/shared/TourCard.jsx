b696f909ae8a1db207c4cc52466b5dec490c2429
godsadeser <ducmanh0801@gmail.com>
2023-11-16 16:39:46 +0700

    Add card body class

--- frontend/src/shared/TourCard.jsx
+++ frontend/src/shared/TourCard.jsx
@@ -15,27 +15,27 @@ const TourCard = ({tour}) => {
             <img src={photo} alt="tour-img" />
             {/* {featured && <span>Featured</span>} */}
         </div>
-        <CardBody className=''>
-        <div className='card__top d-flex align-items-center justify-item-between'>
-            <span className="tour__location d-flex align-items-center gap-1">
-                <i class="ri-map-pin-line"></i> {city}
-            </span>
-            <span className="tour__rating d-flex align-items-center gap-1">
-                <i class="ri-star-fill"></i> {avgRating === 0 ? null : avgRating}
-                {totalRating === 0 ? 'Not rated':(<span>({reviews.length})</span>)}
-            </span>
-        </div>
-        <h5 className="tour__title">
-            <Link to = {`/tours/${_id}`}>{title}</Link>
-        </h5>
-        <div className="card__bottom d-flex align-items-center justify-content-between mt-3">
-            <h5>${price} <span> /person</span></h5>
+        <CardBody className='card__body'>
+            <div className='card__top d-flex align-items-center justify-item-between'>
+                <span className="tour__location d-flex align-items-center gap-1">
+                    <i class="ri-map-pin-line"></i> {city}
+                </span>
+                <span className="tour__rating d-flex align-items-center gap-1">
+                    <i class="ri-star-fill"></i> {avgRating === 0 ? null : avgRating}
+                    {totalRating === 0 ? 'Not rated':(<span>({reviews.length})</span>)}
+                </span>
+            </div>
+            <h5 className="tour__title">
+                <Link to = {`/tours/${_id}`}>{title}</Link>
+            </h5>
+            <div className="card__bottom d-flex align-items-center justify-content-between mt-3">
+                <h5>${price} <span> /person</span></h5>
 
-            <button className="primary__btn booking__btn">
-                <Link to = {`/tours/${_id}`}>Book Now</Link>
-            </button>
-        </div>
-    </CardBody>
+                <button className="primary__btn booking__btn">
+                    <Link to = {`/tours/${_id}`}>Book Now</Link>
+                </button>
+            </div>
+        </CardBody>
     </Card>
     
   </div>
