e708483372c18811b3a3ca9d3acbd8c2ec4abfd0
godsadeser <ducmanh0801@gmail.com>
2023-11-19 01:32:14 +0700

    Add admin search case

--- frontend/src/pages/SearchResultList.jsx
+++ frontend/src/pages/SearchResultList.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useContext, useState} from 'react'
 import CommonSection from '../shared/CommonSection'
 import { Container, Row, Col } from 'reactstrap'
 
@@ -6,11 +6,15 @@ import { useLocation } from 'react-router-dom'
 import TourCard from '../shared/TourCard'
 
 import Newsletter from '../shared/Newsletter'
+import { AuthContext } from '../context/AuthContext'
+import AdminTourCard from '../shared/AdminTourCard'
 
 const SearchResultList = () => {
   const location = useLocation()
 
   const [data] = useState(location.state)
+
+  const {user} = useContext(AuthContext)
 
   return (
     <>
@@ -23,7 +27,21 @@ const SearchResultList = () => {
               ( <h4 className='text-center'>No tours found!</h4>)
               : ( data?.map(tour => (
                 <Col lg='3' className='mb-4' key={tour._id}>
-                  <TourCard tour={tour}/>
+                  {
+                    user?(
+                      <div>
+                        {
+                          user.role === 'admin'?(
+                            <AdminTourCard tour={tour}/>
+                          ):(
+                            <TourCard tour={tour}/>
+                          )
+                        }
+                      </div>
+                    ):(
+                      <TourCard tour={tour}/>                      
+                    )
+                  }
                 </Col>
               )))
             }
