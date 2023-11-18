f53408e7747459af0ee1879d3fc3c780b1a2573f
godsadeser <ducmanh0801@gmail.com>
2023-11-19 00:22:05 +0700

    connect delete api v1

--- frontend/src/shared/AdminTourCard.jsx
+++ frontend/src/shared/AdminTourCard.jsx
@@ -4,10 +4,33 @@ import { Card, CardBody } from 'reactstrap'
 import { Link} from 'react-router-dom'
 import calculateAvgRating from '../utils/avgRating'
 
+import { AuthContext } from '../context/AuthContext'
+import { BASE_URL } from '../utils/config'
+
 const AdminTourCard = ({tour}) => {
     const {_id, city, title, photo, price, featured, reviews} = tour
 
     const {totalRating, avgRating} = calculateAvgRating(reviews)
+
+    const handleClick = async e => {
+        e.preventDefault()
+        if(window.confirm('Are your sure?')){
+            try {
+                const res = await fetch(`${BASE_URL}/tours/${_id}`, {
+                    method:'delete',
+                })
+
+                const result = await res.json()
+                if(! res.ok){
+                    return alert(result.message)
+                }else{
+                    return alert('Deleted Successfully!')
+                }
+            } catch (error) {
+                alert(error.message)
+            }
+        }
+    }
 
   return <div className='tour__card'>
     <Card>
@@ -31,8 +54,8 @@ const AdminTourCard = ({tour}) => {
                 <button className="primary__btn modify__btn">
                     <Link to = {`/tours/${_id}`}>Modify</Link>
                 </button>
-                <button className="primary__btn delete__btn">
-                    <Link to = {`/tours/${_id}`}>Delete</Link>
+                <button className="primary__btn delete__btn" onClick={handleClick}>
+                    Delete
                 </button>
 
             </div>
